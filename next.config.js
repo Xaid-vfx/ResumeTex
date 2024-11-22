b1563d1d432491edb21cefacdee2d48edfa36ba3
Xaid-vfx <mohd.zaid.123786@gmail.com>
2024-11-22 20:57:52 +0530

    vercel fix 7

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build',
-    output: 'export',
     webpack: (config) => {
         config.resolve = {
             ...config.resolve,
